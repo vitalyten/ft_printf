2588bddbf80198c9b904631daf20fde50bccf8ac
VitalyTen <vtenigin@student.42.us.org>
2016-12-25 17:08:57 -0800

    ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: vtenigin <vtenigin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 22:42:58 by vtenigin          #+#    #+#             */
-/*   Updated: 2016/12/08 19:37:52 by vtenigin         ###   ########.fr       */
+/*   Updated: 2016/12/23 21:31:42 by vtenigin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,71 +19,67 @@
 typedef struct		s_env
 {
 	va_list		arg;
-	int			ret;
 	int			i;
-	int			plus;
-	int			minus;
-	int			space;
 	int			hash;
 	int			zero;
-	int			f_none;
+	int			minus;
+	int			plus;
+	int			space;
+	int			none;
+	int			width;
+	int			prec;
+	int			hh;
+	int			h;
 	int			l;
-	int			p;
-	int			m_h;
-	int			m_hh;
-	int			m_l;
-	int			m_ll;
-	int			m_z;
-	int			m_j;
+	int			ll;
+	int			j;
+	int			z;
+	int			ret;
 }					t_env;
 
-/*
-**		tools
-*/
-
-void				ft_putnbru(unsigned int nb);
-char				*ft_utoa_base(unsigned long long value, int base);
-char				*ft_ltoa_base(long long value, int base);
-char				*ft_dtoa_base(double value, int base, int precision,
-		t_arg *a);
-void				ft_repeat_char(char c, int nbr);
-char				*str_lower(char *str);
-void				count_ret(char **to_print, t_arg *a);
-
-/*
-**		type conversion
-*/
-
-void				type_d(char type, t_arg *a);
-void				type_x(char type, t_arg *a);
-void				type_u(char type, t_arg *a);
-void				type_o(char type, t_arg *a);
-void				type_s_ls(char type, t_arg *a);
-void				type_c_lc(char type, t_arg *a);
-void				type_percent(t_arg *a);
-void				type_f(t_arg *a);
-void				type_p(t_arg *a);
-
-/*
-**		flags
-*/
-
-void				check_flags(char *format, t_arg *a);
-void				check_min_lenght(char *format, t_arg *a);
-void				check_precision(char *format, t_arg *a);
-void				set_default_precision(char type, t_arg *a);
-void				check_modifier(char *format, t_arg *a);
-
-/*
-**		unicode
-*/
-
-char				**unicode_masks(char *wc);
-void				masks_fill(char *mask, char *wc);
-void				print_wchar(char **to_print);
-unsigned char		*stock_wchar(char **to_print);
-
-int					ft_printf(char *format, ...);
-void				check_type(char *format, t_arg *a);
+int		ft_printf(char *fmt, ...);
+void	envreset(t_env *env);
+void	setflags(char *fmt, t_env *env);
+int		isflag(char c);
+int		ft_abs(int n);
+void	settype(char *fmt, t_env *env);
+void	invtype(char c, t_env *env);
+char	*sjoinfree(char *s1, char *s2);
+char	*snjoinfree(char *s1, char *s2, size_t len);
+char	*ft_lltoa_base(long long value, int base);
+char	*ft_ulltoa_base(unsigned long long value, int base);
+void	xconv(char type, t_env *env);
+char	*xconvstd(unsigned long long x, t_env *env);
+char	*xconvzero(unsigned long long x, t_env *env);
+void	oconv(char type, t_env *env);
+char	*oconvstd(unsigned long long o, t_env *env);
+char	*oconvzero(unsigned long long o, t_env *env);
+int		iswidth(char c);
+void	setwidth(char *fmt, t_env *env);
+void	setprec(char *fmt, t_env *env);
+int		ismod(char c);
+void	setmod(char *fmt, t_env *env);
+void	slsconv(char type, t_env *env);
+int		ishead(char c);
+void	trimucs(char *str);
+void	lsconv(t_env *env);
+void	sconv(t_env *env);
+char	**getunicode(char *bin);
+char	*fillutf(char *utf, char *bin);
+void			printuc(char **uc);
+unsigned char	atocbin(char *str);
+char	*ucstostr(wchar_t *ucs);
+void	clcconv(char type, t_env *env);
+void	cconv(t_env *env);
+void	free2d(char **strs);
+void	retcount(char **strs, t_env *env);
+void	percentconv(t_env *env);
+void	pconv(t_env *env);
+void	uconv(char type, t_env *env);
+char	*uconvstd(unsigned long long u, t_env *env);
+char	*uconvzero(unsigned long long u, t_env *env);
+void	dconv(char type, t_env *env);
+char	*dconvstd(long long d, t_env *env);
+char	*dconvzero(long long d, t_env *env);
 
 #endif
